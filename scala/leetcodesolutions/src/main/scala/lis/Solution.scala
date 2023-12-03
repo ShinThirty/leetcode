4f113289c0a14366fa5d41ae0cecd64abf93f727
Lingnan Liu <xmxu00@gmail.com>
2023-12-03 13:48:29 -0800

    add solution to 2918

--- scala/leetcodesolutions/src/main/scala/lis/Solution.scala
+++ scala/leetcodesolutions/src/main/scala/lis/Solution.scala
@@ -28,4 +28,20 @@ object Solution {
 
     num.foldLeft(0)(accu)
   }
+
+  def minSum(nums1: Array[Int], nums2: Array[Int]): Long = {
+    val zeroes1 = nums1.count(_ == 0)
+    val zeroes2 = nums2.count(_ == 0)
+
+    val minSum1 = nums1.foldLeft(0L)((b, a) => b + a) + zeroes1
+    val minSum2 = nums2.foldLeft(0L)((b, a) => b + a) + zeroes2
+
+    if (
+      (zeroes1 == 0 && minSum1 < minSum2) || (zeroes2 == 0 && minSum2 < minSum1)
+    ) {
+      -1L
+    } else {
+      Math.max(minSum1, minSum2)
+    }
+  }
 }
